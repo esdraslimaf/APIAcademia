9e6eaf39bc99a25099a6b7b3c819fd7811a4bea3
esdraslimaf <esdraslimaf@gmail.com>
2024-01-09 22:54:09 -0300

    Criada UserEntity e Configurações da Camada Data

---
+++ Api.Data/Mapping/UserMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Data.Mapping
+{
+    internal class UserMap
+    {
+    }
+}
