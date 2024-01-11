0054d0cca3a09bb439d32650f6da25ee51f784d3
esdraslimaf <esdraslimaf@gmail.com>
2024-01-10 23:54:52 -0300

    Camada Service - Criada a IUserService na Domain e UserService na Service

---
+++ Api.Service/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Api.Domain.Interfaces.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Service.Services
+{
+    internal class UserService : IUserService
+    {
+        private readonly IBaseRepository<UserEntity> _repo;
+
+        public UserService(IBaseRepository<UserEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repo.DeleteAsync(id);
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _repo.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _repo.SelectAllAsync();
+        }
+
+        public async Task<UserEntity> Post(UserEntity user)
+        {
+            return await _repo.InsertAsync(user);
+        }
+
+        public async Task<UserEntity> Put(UserEntity user)
+        {
+            return await _repo.UpdateAsync(user);
+        }
+    }
+}
