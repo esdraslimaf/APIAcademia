8add03533ffb27ca5876a43611661c1516eadd0d
esdraslimaf <esdraslimaf@gmail.com>
2024-01-31 04:09:46 -0300

    Adicionados UserDto, UserDtoCreateResult e UserDtoUpdateResult

--- Api.Domain/Dtos/User/UserDto.cs
+++ Api.Domain/Dtos/User/UserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Api.Domain.Dtos.User
 {
     public class UserDto
     {
+
+        [Required(ErrorMessage = "Campo nome é obrigatório!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Campo email é obrigatório!")]
+        [EmailAddress(ErrorMessage = "O email está em um formato inválido!")]
+        public string Email { get; set; }
     }
 }
